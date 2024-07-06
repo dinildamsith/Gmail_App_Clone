ba9267b885e6394e1435b8ca42a1e80359395aab
dinildamsith <dinildamsith52@gmail.com>
2024-07-06 19:13:39 +0530

    add talwind css and configurations

--- Gmail_Mobile_App/app/index.tsx
+++ Gmail_Mobile_App/app/index.tsx
@@ -2,8 +2,8 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View>
-      
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Hello, Tailwind CSS!</Text>
     </View>
   );
 }
